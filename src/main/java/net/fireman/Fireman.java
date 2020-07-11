1a269934d4d958b95cfbc50c4e1bae21adea5aa7
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2020-07-12 11:03:25 +1200

    Initial commit

---
+++ src/main/java/net/fireman/Fireman.java
@@ -0,0 +1,13 @@
+package net.fireman;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Fireman implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
